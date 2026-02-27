2fb0a8e2dfe7b431cd3bb9b0139b6d30ce45cc83
Cawe Alves <cawe.wewe04@gmail.com>
2026-02-27 12:59:56 -0300

    feat(ui): create avatars component with dynamic tooltips

---
+++ resources/js/components/lp/avatars.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const AVATARS = [
+    "https://api.dicebear.com/9.x/adventurer/svg?seed=Sawyer",
+    "https://api.dicebear.com/9.x/adventurer/svg?seed=George",
+    "https://api.dicebear.com/9.x/adventurer/svg?seed=Jack",
+    "https://api.dicebear.com/9.x/adventurer/svg?seed=Luis",
+    "https://api.dicebear.com/9.x/adventurer/svg?seed=Mason",
+];
+
+const TEXTS = [
+    "...",
+    "...?",
+    "?",
+];
+
+</script>
+
+<template>
+    <span v-for="avatar in AVATARS.length" :key="avatar">
+        <UTooltip arrow defaultOpen :text="TEXTS[Math.floor(Math.random() * TEXTS.length)]" :content="{
+            side: 'top'
+        }" :ui="{
+            content: 'surface-elevated surface-elevated-interactive font-mono'
+        }">
+            <UAvatar class="surface-elevated surface-elevated-interactive size-12" :src="AVATARS[avatar - 1]" />
+        </UTooltip>
+    </span>
+</template>
