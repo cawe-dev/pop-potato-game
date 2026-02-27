2584247c181e38e084a1557b48cda81b267f131c
Cawe Alves <cawe.wewe04@gmail.com>
2026-02-27 16:43:27 -0300

    chore(avatars): define types and constants for floating avatars

---
+++ resources/js/types/floatAvatars.d.ts
@@ -0,0 +1,11 @@
+interface FloatAvatar {
+    id: number;
+    localeX: number;
+    localeY: number;
+    speed: number;
+    delay: number;
+    scale: number;
+    src: string;
+}
+
+export type { FloatAvatar };
