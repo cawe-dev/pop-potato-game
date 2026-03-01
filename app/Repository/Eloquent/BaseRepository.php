08dc42201f464e5a7f409606f7cc653fc48f4646
Cawe Alves <cawe.wewe04@gmail.com>
2026-03-01 16:59:10 -0300

    refactor(utils): make BaseRepository abstract and methods final

--- app/Repository/Eloquent/BaseRepository.php
+++ app/Repository/Eloquent/BaseRepository.php
@@ -9,26 +9,26 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
 
-final class BaseRepository implements IBaseRepository
+abstract class BaseRepository implements IBaseRepository
 {
     public function __construct(private Model $model) {}
 
-    public function find(int $id)
+    final public function find(int $id)
     {
         return $this->model->find($id);
     }
 
-    public function findAll()
+    final public function findAll()
     {
         return $this->model->all();
     }
 
-    public function create(array $data)
+    final public function create(array $data)
     {
         return $this->model->create($data);
     }
 
-    public function update(int $id, array $data)
+    final public function update(int $id, array $data)
     {
         $record = $this->find($id);
         $record->update($data);
@@ -36,14 +36,14 @@ final class BaseRepository implements IBaseRepository
         return $record;
     }
 
-    public function delete(int $id)
+    final public function delete(int $id)
     {
         $record = $this->find($id);
 
         return $record->delete();
     }
 
-    public function findAllPaginated(int $perPage): LengthAwarePaginator
+    final public function findAllPaginated(int $perPage): LengthAwarePaginator
     {
         return $this->paginateQuery($this->model->query(), $perPage);
     }
