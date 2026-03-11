9cdf7398c0d25aca4981ab3af6957ea6bcb7fcfc
Cawe Alves <cawe.wewe04@gmail.com>
2026-03-11 13:27:00 -0300

    refactor(room): use pipe operator in service

--- app/Services/Room/RoomService.php
+++ app/Services/Room/RoomService.php
@@ -4,37 +4,66 @@ declare(strict_types=1);
 
 namespace App\Services\Room;
 
+use App\Actions\room\JoinRoom;
 use App\Enums\Room\RoomTheme;
 use App\Enums\Room\RoomType;
 use App\Repository\Eloquent\Room\IRoomRepository;
 use App\Services\BaseService;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 final class RoomService extends BaseService implements IRoomService
 {
-    public function __construct(IRoomRepository $repository)
+    public function __construct(IRoomRepository $repository, private JoinRoom $join)
     {
         parent::__construct($repository);
     }
 
     protected function beforeStore(array $data): array
     {
-        $password = $data['password'];
+        $data = $data
+        |> $this->clearPassword(...)
+        |> $this->alterIcon(...);
 
-        if ($data['type'] !== RoomType::PRIVATE->value) {
-            $password = null;
-        }
+        return [
+            ...$data,
+            'code' => $this->generateRoomCode(),
+        ];
+    }
 
-        return array_merge($data, [
-            'code'     => $this->generateRoomCode(),
-            'icon'     => RoomTheme::from($data['theme'])->icon(),
-            'password' => $password,
-        ]);
+    protected function afterStore(Model $model, array $data): void
+    {
+        ($this->join)($model, $data['user_id']);
+    }
+
+    protected function beforeUpdate(array $data): array
+    {
+        return $data
+        |> $this->clearPassword(...)
+        |> $this->alterIcon(...);
     }
 
     private function generateRoomCode(): string
     {
         return Str::random(4)
         |> Str::upper(...);
+    }
+
+    private function clearPassword(array $data): array
+    {
+        if (isset($data['type']) && $data['type'] !== RoomType::PRIVATE->value) {
+            $data['password'] = null;
+        }
+
+        return $data;
+    }
+
+    private function alterIcon(array $data): array
+    {
+        if (isset($data['theme'])) {
+            $data['icon'] = RoomTheme::from($data['theme'])->icon();
+        }
+
+        return $data;
     }
 }
